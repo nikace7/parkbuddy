a4ac85d2d9de99fb4332a05d7ed4a35e24a5cf22
nikace7 <khatrinikace@gmail.com>
2024-09-25 13:45:00 +0545

    Update(feat): GeoCoding Fetch_location_data

--- theme/models.py
+++ theme/models.py
@@ -57,24 +57,31 @@ class ParkingPlace(models.Model):
     def __str__(self):
         return f'{self.latitude}, {self.longitude} - {self.address}'
 
+def fetch_location_data(latitude, longitude):
+    api_url = f"https://geocode.maps.co/reverse?lat={latitude}&lon={longitude}"
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed with status code {response.status_code}")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    return None
+
+
 # Static method to handle pre-save signals
 @receiver(pre_save, sender=ParkingPlace)
 def populate_location_details(sender, instance, **kwargs):
     if not instance.address or not instance.city or not instance.country:
-        try:
-            # Perform the reverse geocoding to get location details
-            response = requests.get(
-                f"https://geocode.maps.co/reverse?lat={instance.latitude}&lon={instance.longitude}"
-            )
-            data = response.json()
+        data = fetch_location_data(instance.latitude, instance.longitude)
+        if data and "address" in data:
+            address_data = data['address']
+            instance.address = address_data.get('suburb', '') or \
+                               address_data.get('neighbourhood', '') or \
+                               address_data.get('city_district', '')
+            instance.city = address_data.get('city', '')
+            instance.country = address_data.get('country', '')
 
-            if "address" in data:
-                # Populate fields based on the geocoding response
-                instance.address = data['address'].get('suburb', '') or \
-                                   data['address'].get('neighbourhood', '') or \
-                                   data['address'].get('city_district', '')
-                instance.city = data['address'].get('city', '')
-                instance.country = data['address'].get('country', '')
-
-        except Exception as e:
-            print(f"Error retrieving location details: {e}")
+        if not instance.address or not instance.city or not instance.country:
+            print(f"Failed to fetch location data for ({instance.latitude}, {instance.longitude})")
