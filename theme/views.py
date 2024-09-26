e092c88427fe7b1990941338779fd5bdb0d03a40
nikace7 <khatrinikace@gmail.com>
2024-09-26 15:39:42 +0545

    adding: Path for npm in settings.py

--- theme/views.py
+++ theme/views.py
@@ -33,7 +33,7 @@ def save_parking_place(request):
 
 
 def send_otp(phone_number, otp):
-    # You can integrate an actual SMS service here to send the OTP
+  
     print(f"OTP {otp} sent to {phone_number}")
 
 def login_view(request):
@@ -41,12 +41,12 @@ def login_view(request):
         phone_number = request.POST.get('phone')
 
         if phone_number and len(phone_number) == 10:
-            otp = random.randint(100000, 999999)  # Generate a 6-digit OTP
+            otp = random.randint(100000, 999999) 
             request.session['phone_number'] = phone_number
             request.session['otp'] = otp
-            send_otp(phone_number, otp)  # Mock sending OTP
+            send_otp(phone_number, otp)  
 
-            return redirect('login_otp')  # Redirect to OTP input page
+            return redirect('login_otp')  
         else:
             messages.error(request, "Please enter a valid 10-digit phone number.")
 
@@ -59,11 +59,8 @@ def login_otp_view(request):
         session_otp = request.session.get('otp')
 
         if otp_input and otp_input == str(session_otp):
-            # OTP is correct, login the user
-            # Here, you would log in or create the user based on phone number
-            # For example: User.objects.get_or_create(phone_number=phone_number)
             messages.success(request, "Login successful!")
-            return redirect('homepage')  # Redirect to homepage
+            return redirect('homepage')  
         else:
             messages.error(request, "Invalid OTP. Please try again.")
 
