53b104ed6b9586ddbbece43d6ce9b03a4aaa39ff
nikace7 <khatrinikace@gmail.com>
2024-10-24 12:18:00 +0545

    feat(Parking Booking, Slot, Vehicle): models added for mentioned feats

--- theme/models.py
+++ theme/models.py
@@ -14,7 +14,12 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.phone_number
-    
+
+class Vehicle(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vehicle_type = models.CharField(max_length=100)
+    registration_number = models.CharField(max_length=20)
+
 #Parking Space model to store parking locations
 
 class ParkingSpace(models.Model):
@@ -33,7 +38,25 @@ class ParkingSlot(models.Model):
     released_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.parking_space.name} - {self.user.username} - {self.booked_at}"    
+        return f"{self.parking_space.name} - {self.user.username} - {self.booked_at}"
+    
+class ParkingBooking(models.Model):
+    space = models.ForeignKey(ParkingSpace, on_delete=models.CASCADE)
+    slot = models.ForeignKey(ParkingSlot, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+
+    arriving_at = models.DateTimeField()
+    exiting_at = models.DateTimeField()
+    
+    is_paid = models.BooleanField(default=False)    
+
+class Payment(models.Model):
+    slot = models.OneToOneField(ParkingSlot, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    paid_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.slot} - {self.amount} - {self.paid_at}"    
 
 # Function to find nearest parking spaces
 def find_nearest_parking_spaces(user_latitude, user_longitude, max_distance_km=2, vehicle_type=None):
