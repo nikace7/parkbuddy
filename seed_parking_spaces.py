0e546376ecb783502385a1356c37454f515ea7b3
nikace7 <khatrinikace@gmail.com>
2025-01-19 07:29:58 +0545

    seeding for databse entry

---
+++ seed_parking_spaces.py
@@ -0,0 +1,27 @@
+# from parkbuddy.theme.models import ParkingSpace
+# from django.contrib.auth import get_user_model
+
+# User = get_user_model()
+
+# # Create a parking manager
+
+# User.objects.create(
+#     username='9876543210',
+
+# )
+
+# # Create 20 parking spaces for the parking manager
+
+# ParkingSpace.objects.create(
+#     user=User.objects.get(username='9876543210'),
+#     name='Parking Space 1',
+#     location='SRID=4326;POINT(77.5946 12.9716)',
+#     vehicle_type='Car',
+#     location_name='MG Road',
+#     price_per_hr=50,
+#     price_per_half_hr=30,
+#     info='This is a parking space on MG Road',
+# )
+
+
+print("Hello, World!")
